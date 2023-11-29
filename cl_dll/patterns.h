79305a0393b3620e75c022919eb1613d0fb00b8b
BlueNightHawk <46296596+BlueNightHawk@users.noreply.github.com>
2023-11-29 12:40:50 +0530

    Fix build error

--- cl_dll/patterns.h
+++ cl_dll/patterns.h
@@ -13,11 +13,11 @@ public:
 
 namespace patterns
 {
-Pattern EmitWaterPolys = {
+const Pattern EmitWaterPolys = {
 	"EmitWaterPolys",
 	"\x55\x8B\xEC\x83\xEC\x00\xA1\x00\x00\x00\x00\x33\xC5\x89\x45\x00\x56\xF3\x0F\x10\x15", 
 	"xxxxx?x????xxxx?xxxxx"};
-Pattern gl_texturemode_hook_callback = {
+const Pattern gl_texturemode_hook_callback = {
 	"gl_texturemode_hook_callback",
 	"\x55\x8B\xEC\x8B\x45\x00\x85\xC0\x0F\x84\x00\x00\x00\x00\x8B\x40",
 	"xxxxx?xxxx????xx"};
